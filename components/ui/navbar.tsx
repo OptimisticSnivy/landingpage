6a9e13cef7a80cfe96dec19e0c33300260faa3b2
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-10-31 14:44:32 +0530

    Home layout page decided

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,15 +3,14 @@ import Link from "next/link"
 export default function Nav() {
 	return (
 		<div className="text-[#ebc06d] font-regular text-lg">
-			<div className="flex flex-row justify-center mt-[30px] min-w-[80%] text-[20px]">
-				<h1> Anand Mahamuni </h1>
-				<div className="flex flex-row ml-100">
-					<h1 className="mr-3"><Link href="/">home</Link></h1>
-					<h1 className="mr-3"><Link href="/about">about</Link></h1>
-					<h1><Link href="/contact">contact</Link></h1>
+			<div className="flex flex-row mt-[30px] min-w-[80%] text-[20px]">
+				{/* <h1 className="italic"> anand mahamuni </h1> */}
+				<div className="flex flex-row ml-130 mt-30">
+					<h1 className="mr-3"><Link className="hover:text-[#7f91b2]" href="/">home</Link></h1>
+					<h1 className="mr-3"><Link className="hover:text-[#7f91b2]" href="/">about</Link></h1>
+					<h1 className="mr-3"><Link className="hover:text-[#7f91b2]" href="/">contact</Link></h1>
 				</div>
 			</div>
 		</div>
 	);
-
 }
