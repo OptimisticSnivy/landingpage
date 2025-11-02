a5952038ac6031c205ae0b5bdd3be2cc75ec7665
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-11-02 10:04:07 +0530

    Cleanup

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,11 @@
 import { herbert } from "../fonts"
+import Welcome from "./../content/posts/welcome.md"
 
 export default function Page() {
 	return (
-		<div className="flex flex-col text-[#ebc06d] font-regular mt-10 w-2xl mx-auto">
-			<h1 className={`${herbert.className} text-4xl`}> Coming Soon! </h1>
+		<div className="prose flex flex-col text-[#ebc06d] font-regular mt-10 w-2xl mx-auto">
+			<Welcome />
+			{/* <h1 className={`${herbert.className} text-4xl`}> Coming Soon! </h1> */}
 		</div>
 	)
 }
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
+import createMDX from '@next/mdx'
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	/* config options here */
+	reactCompiler: true,
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
+const withMDX = createMDX({
+	// Add markdown plugins here, as desired
+	extension: /\.(md|mdx)$/,
+	options: {
+		remarkPlugins: [["remark-gfm", { strict: true, throwOnError: true }]],
+	},
+})
 
-export default nextConfig;
+export default withMDX(nextConfig);
